43cdd2890478763e77d0a85e760daccbb00bb89f
fwttnnn <lolywk@tutanota.com>
2026-03-23 18:56:19 +0700

    feat(bash/app): universal build system

---
+++ app.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+
+COMPRESSION=nil
+BUILD=false
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --build)
+      BUILD=true
+      shift
+      ;;
+    --compress)
+      COMPRESSION=${2:--7}
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+if $BUILD; then
+  sbcl --non-interactive \
+       --eval '((lambda ()
+                  (require :asdf)
+                  (load "~/quicklisp/setup.lisp")
+                  (push #p"./" asdf:*central-registry*)))' \
+       --eval '(ql:quickload :app)' \
+       --eval "(sb-ext:save-lisp-and-die
+                 \"app\"
+                 :toplevel #'app:main
+                 :compression $COMPRESSION
+                 :executable t
+                 :save-runtime-options t)"
+  exit
+fi
+
+sbcl --non-interactive \
+     --eval '((lambda ()
+               (require :asdf)
+               (load "~/quicklisp/setup.lisp")
+               (push #p"./" asdf:*central-registry*)))' \
+     --eval '(ql:quickload :app)' \
+     --eval '(app:main)'
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-sbcl --non-interactive \
-     --eval '((lambda ()
-                (require :asdf)
-                (load "~/quicklisp/setup.lisp")
-                (push #p"./" asdf:*central-registry*)))' \
-     --eval '(ql:quickload :app)' \
-     --eval '(app:main)' \
