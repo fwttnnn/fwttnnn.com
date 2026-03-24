8cec5a0acc4d291a0fd496d5683358b947d54c31
fwttnnn <lolywk@tutanota.com>
2026-03-24 07:34:28 +0700

    feat(lisp/html5): add script tags, append \!DOCTYPE

--- src/web/html5.lisp
+++ src/web/html5.lisp
@@ -3,9 +3,14 @@
   (:export #:render))
 
 (defmacro html5:render (title &body body)
-  `(let ((spinneret:*html-style* :tree))
-     (format t "~a~%"
-       (spinneret:with-html-string
-         (:html
-          (:head (:title ,title))
-          (:body ,@body))))))
+  `(progn
+     (format t "<!DOCTYPE html>~%")
+     (let ((spinneret:*html-style* :tree))
+       (format t "~a~%"
+         (spinneret:with-html-string
+           (:html
+            (:head
+             (:title ,title)
+             (:script :src "https://unpkg.com/@studio-freight/lenis@1.0.42/dist/lenis.min.js")
+             (:script :src "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js")))
+            (:body ,@body))))))
