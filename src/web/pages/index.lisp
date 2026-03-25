f0176914885ef4ad5f580a0e79f4e20629ade033
fwttnnn <lolywk@tutanota.com>
2026-03-25 07:21:41 +0700

    chore(pages/index): low on spacing

--- src/web/pages/index.lisp
+++ src/web/pages/index.lisp
@@ -6,8 +6,8 @@
   (html5:render
     (:p "Hi, welcome to my little corner on the internet!")
     (:ul
-     (loop for item in data/steam/wishlist:+data+ do
-       (:li (getf item :category)
-         (:ul
-           (loop for link in (getf item :links) do
-             (:li (:a :href link link)))))))))
+      (loop for item in data/steam/wishlist:+data+ do
+        (:li (getf item :category)
+          (:ul
+            (loop for link in (getf item :links) do
+              (:li (:a :href link link)))))))))
