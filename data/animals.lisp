29c87fcd05d0e274ddcd5349122c79841375cbd2
fwttnnn <lolywk@tutanota.com>
2026-08-16 04:21:10 +0700

    chore(data/animals): whip spider, house centipede, bats

--- data/animals.lisp
+++ data/animals.lisp
@@ -3,10 +3,26 @@
   (:export #:+data+))
 
 (defparameter data/animals:+data+
-  '((:name "Crows"
+  '((:name "Crow"
      :images '("https://i.pinimg.com/736x/6d/04/a8/6d04a82c85bee54c5d6fda3d19e9d0e2.jpg"
                "https://i.pinimg.com/736x/f8/0c/32/f80c323d56643e4a25f8585eac864e4d.jpg"
                "https://i.pinimg.com/vwebp/736x/19/fd/d3/19fdd36c88cc2f79b831fe8c25c229f8.webp"))
-    (:name "Orcas"
+    (:name "Orca"
      :images '("https://i.pinimg.com/vwebp/1200x/f8/33/5b/f8335b799b00b74953bdfc3aa751a065.webp"
-               "https://i.pinimg.com/vwebp/736x/4f/fd/db/4ffddbe2b9d9a3628b493266518dabd1.webp"))))
+               "https://i.pinimg.com/vwebp/736x/4f/fd/db/4ffddbe2b9d9a3628b493266518dabd1.webp"))
+    (:name "Whip Spider"
+     :images '("https://i.pinimg.com/736x/a0/16/50/a0165068a2b4744b44de73dd2a9e9ba3.jpg"
+               "https://i.pinimg.com/736x/01/92/01/0192016bcdb1523770fa159b8b237e51.jpg"))
+    (:name "House Centipede"
+     :images '("https://i.pinimg.com/vwebp/736x/e5/90/65/e590655e71c958b7bec5c16207fd26cd.webp"
+               "https://i.pinimg.com/vwebp/1200x/4b/12/36/4b123635f187a9bc40bd200ec0e6f80f.webp"
+               "https://i.pinimg.com/736x/34/d2/8d/34d28d2eebec41b55330eeff546852a3.jpg"
+               "https://i.pinimg.com/vwebp/736x/12/43/6d/12436de6530022103e60e27e5486cbdb.webp"))
+    (:name "Bats"
+     :images '("https://i.pinimg.com/736x/2f/2f/eb/2f2feb35af8c59ddca055d7d5bf3ce89.jpg"
+               "https://i.pinimg.com/736x/87/12/80/8712807d9ed15717773a76beb6b574a2.jpg"
+               "https://i.pinimg.com/736x/3a/67/f5/3a67f555f2fdb9615c8e514bdab130d6.jpg"
+               "https://i.pinimg.com/1200x/43/3e/c3/433ec37e278dd154a9cde87508130492.jpg"
+               "https://i.pinimg.com/736x/55/11/9f/55119f1f692a1ab1279c2b096a13e0bc.jpg"
+               "https://i.pinimg.com/736x/ea/f5/2a/eaf52ad20107a04862619dcca59a58f2.jpg"
+               "https://i.pinimg.com/1200x/b5/00/d3/b500d30548452aeafff9c926a7ac555f.jpg"))))
