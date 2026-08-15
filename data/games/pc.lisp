41f2bbb17896ac3504cb50445a52df69ca988da5
fwttnnn <lolywk@tutanota.com>
2026-08-16 04:16:42 +0700

    chore(data/games): add incrementals

--- data/games/pc.lisp
+++ data/games/pc.lisp
@@ -3,4 +3,7 @@
   (:export #:+data+))
 
 (defparameter data/games/pc:+data+
-  '())
+  '((:genre "Incremental"
+     :games ((:name "A Dark Room"
+              :link "https://adarkroom.doublespeakgames.com/"
+              :code "https://github.com/doublespeakgames/adarkroom")))))
