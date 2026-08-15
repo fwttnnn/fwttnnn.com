cf6fb31a7f5c83face45a8c39caa5f43ffc802d3
fwttnnn <lolywk@tutanota.com>
2026-08-16 04:27:14 +0700

    chore(components/animated): link spacing

--- src/components/animated/link.lisp
+++ src/components/animated/link.lisp
@@ -6,12 +6,12 @@
   (sta6:html
     (when icon
       (:a :style "display: inline-flex; align-items: center; vertical-align: middle;"
-             :href href
-             :target "_blank"
-             :rel "noreferrer noopener"
-             (:img :src icon
-                   :width 20
-                   :height 20))
+          :href href
+          :target "_blank"
+          :rel "noreferrer noopener"
+          (:img :src icon
+                :width 20
+                :height 20))
       (:raw "&nbsp;"))
     (:a :href href
         :target "_blank"
