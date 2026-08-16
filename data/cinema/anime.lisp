8f92cb0de1bbf835e06e68fd4cde633cb5aecbc9
fwttnnn <lolywk@tutanota.com>
2026-08-16 22:11:13 +0700

    chore(data/cinema): fill empty fields

--- data/cinema/anime.lisp
+++ data/cinema/anime.lisp
@@ -5,15 +5,12 @@
 (defparameter data/cinema/anime:+data+
   '((:genre "Game Adaptation"
      :animes ((:name "Devil May Cry: The Animated Series"
-               :publisher ""
-               :poster ""
-               :link "")
-              (:name ""
-               :publisher ""
-               :poster ""
-               :link "")))
+               :poster "https://upload.wikimedia.org/wikipedia/en/e/e4/Devil_May_Cry_vol_1.jpg"
+               :link "http://dmc-tv.com")))
     (:genre "Adventure"
      :animes ((:name "Made in Abyss"
-               :publisher ""
-               :poster ""
-               :link "")))))
+               :poster "https://upload.wikimedia.org/wikipedia/en/9/9a/Made_in_Abyss_volume_1_cover.jpg"
+               :link "https://miabyss.com/")
+              (:name "Blue Exorcist"
+               :poster "https://upload.wikimedia.org/wikipedia/en/6/62/Blue_Exorcist_vol_1.png"
+               :link "http://ao-ex.com/")))))
--- data/cinema/series.lisp
+++ data/cinema/series.lisp
@@ -3,4 +3,7 @@
   (:export #:+data+))
 
 (defparameter data/cinema/series:+data+
-  '())
+  '((:genre "Law"
+     :movies ((:name "Better Call Saul"
+               :link "https://en.wikipedia.org/wiki/Better_Call_Saul"
+               :poster "https://upload.wikimedia.org/wikipedia/en/1/1c/Better_Call_Saul_season_1.jpg")))))
